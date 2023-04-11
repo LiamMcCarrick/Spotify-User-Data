bbd17ff298bd9bd183e56ec12004e3fb9c39d653
Liam McCarrick <wrmccarrick@gmail.com>
2023-04-10 23:50:10 -0400

    updated .gitignore and added data pull file

---
+++ spotify_data_pull.py
@@ -0,0 +1,22 @@
+import json
+import requests
+from secrets_key import CLIENT_ID, CLIENT_SECRET
+
+client_id = CLIENT_ID
+client_secret = CLIENT_SECRET
+
+#get streaming history
+def get_streaming_history():
+    pass
+
+#get authentication token
+def token():
+    pass
+
+#get track id
+def get_track_id():
+    pass
+
+#get track info: genre? features?
+def get_track_info():
+    pass
